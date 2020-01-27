878704fbdf6d87a0ef50045d4d13d4520d45181d
Vlad <izub1995@ukr.net>
2020-01-27 16:22:33 +0200

    start

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+var scene = document.getElementById('scene');
+var promo = document.getElementById('promo');
+var man = document.getElementById('man');
+var parallaxInstance = new Parallax(scene);
+var parallaxman = new Parallax(man);
+var parallaxPromo = new Parallax(promo);
