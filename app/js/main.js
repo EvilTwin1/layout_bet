1481a0f8efdf06f183ac815d535c076b6de7df76
Vlad <izub1995@ukr.net>
2020-01-31 00:24:21 +0200

    fix important property

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 var scene = document.getElementById('scene');
+var scenee = document.getElementById('scenee');
 var promo = document.getElementById('promo');
 var man = document.getElementById('man');
 var parallaxInstance = new Parallax(scene);
+var parallaxscenee = new Parallax(scenee);
+
 var parallaxman = new Parallax(man);
 var parallaxPromo = new Parallax(promo);
